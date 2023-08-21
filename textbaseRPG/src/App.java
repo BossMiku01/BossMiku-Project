4cdc15eb9da026c2d1f414c1da61d1676bfd72ef
BossMiku01 <napizajonas@gmail.com>
2023-08-21 21:38:52 +0800

    Add Character Selection

--- textbaseRPG/src/App.java
+++ textbaseRPG/src/App.java
@@ -5,35 +5,11 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
-        boolean done = false;
-        // Create a new player instance
-        Player player = new Player();
 
-        // Character creation
-        System.out.println("Enter your character's name: ");
-        String playerName = input.nextLine();
-        player.SetName(playerName);
-
-        System.out.println("Choose your class: ");
-        System.out.println("1) Mage 2) Wizard 3) Sage");
-        int classChoice = input.nextInt();
-
-        switch (classChoice) {
-            case 1:
-                player.SetClass("Mage");
-                break;
-            case 2:
-                player.SetClass("Wizard");
-                break;
-            case 3:
-                player.SetClass("Sage");
-                break;
-            default:
-
-                break;
-        }
-
+        Player.CreatCharacter(); //Creating Character
         
+        boolean done = false;
+
         while(!done) {
 
             Map.OutputPosition(); // Reveals where are you.
--- textbaseRPG/src/Player.java
+++ textbaseRPG/src/Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Player {
 
     private String pName;
@@ -8,76 +10,195 @@ public class Player {
     private int pDex; // add acc.
     private int pAgi; // add speed
     private int pArmor;
-    private String pWeapons;
     private int pLvl;
     private int pExp;
-    private int pNextExplvl;
+    private int pNextExpLvl;
     private int pHealthPoints;
     private int pMaxHP;
     private int pManaPoints;
     private int pMaxMP;
-    
+
+    public static void CreatCharacter() {
+
+        Scanner Characterinput = new Scanner(System.in);
+        // Create a new player instance
+        Player newPlayer = new Player();
+
+        // Character creation
+        System.out.println("Enter your character's name: ");
+        String playerName = Characterinput.nextLine();
+        newPlayer.SetName(playerName);
+
+        System.out.println("Choose your class: ");
+        System.out.println("1) Mage 2) Wizard 3) Sage");
+        int classChoice = Characterinput.nextInt();
+
+        switch (classChoice) {
+            case 1:
+                newPlayer.SetClass("Mage");
+                newPlayer.SetInt(15);
+                newPlayer.SetVit(10);
+                newPlayer.SetDex(10);
+                newPlayer.SetAgi(10);
+                newPlayer.SetArmor(10);
+                newPlayer.SetLevel(1);
+                newPlayer.SetExp(0);
+                newPlayer.SetNextExpLvl(1000);
+                newPlayer.SetHP(100);
+                newPlayer.SetMaxHP(100);
+                newPlayer.SetMP(50);
+                newPlayer.SetMaxMP(50);
+                break;
+            case 2:
+                newPlayer.SetClass("Wizard");
+                //STATS
+                break;
+            case 3:
+                newPlayer.SetClass("Sage");
+                //STATS
+                break;
+            default:
+
+                break;
+        }
+        Characterinput.close();
+    }
 
     public String GetName() {
         return pName;
     }
-
     public void SetName(String newName) {
         pName = newName;
     }
 
+
     public String GetClass() {
         return pClass;
     }
-
     public void SetClass(String newClass) {
         pClass = newClass;
     }
 
+
     public int GetGold() {
         return pGold;
     }
+    public void SetGold(int addGold) {
+        pGold += addGold;
+    }
+
 
     public int GetInt() {
         return pInt;
     }
+    public void SetInt(int addInt) {
+        pInt += addInt;
+    }
+
 
     public int GetVit() {
         return pVit;
     }
+    public void SetVit(int addVit) {
+        pVit += addVit;
+    }
+
 
     public int GetDex() {
         return pDex;
     }
+    public void SetDex(int addDex) {
+        pDex += addDex;
+    }
+
 
     public int GetAgi() {
         return pAgi;
     }
+    public void SetAgi(int addAgi) {
+        pAgi += addAgi;
+    }
+
 
     public int GetArmor() {
         return pArmor;
     }
-
+    public void SetArmor(int addArmor) {
+        pArmor += addArmor;
+    }
     public void EquipArmor(int EquipedArmor) {
         pArmor += EquipedArmor;
     }
+
 
     public void EquipWeapon(int EquipedWeapon) {
         pInt += EquipedWeapon;
     }
 
+
     public int GetLevel() {
         return pLvl;
     }
+    public void SetLevel(int setLevel) {
+        pLvl += setLevel;
+    }
+    public void LevelUp() {
+
+        if (pExp >= pNextExpLvl) {
+
+            pLvl++; // lvlup
+            pNextExpLvl = pExp * pExp * 1000; //More Exp for next level
+            pHealthPoints = pMaxHP; //Become Ful health if levelup
+        }
+        
+    }
+
 
     public int GetExp() {
         return pExp;
     }
-
-    public int GetMaxExp() {
-        return pNextExplvl;
+    public void SetExp(int exp) {
+        pExp += exp;
     }
 
+    public int GetNextExpLvl() {
+        return pNextExpLvl;
+    }
+    public void SetNextExpLvl(int nextExpLvl) {
+        pNextExpLvl += nextExpLvl;
+    }
+
+
+    public int GetHP() {
+        return pHealthPoints;
+    }
+    public void SetHP(int addHP) {
+        pHealthPoints += addHP;
+    }
+
+    public int GetMaxHP() {
+        return pMaxHP;
+    }
+    public void SetMaxHP(int addMaxHP) {
+        pMaxHP += addMaxHP;
+    }
+    
+
+
+    public int GetMP() {
+        return pManaPoints;
+    }
+    public void SetMP(int addMP) {
+        pMaxMP += addMP;
+    }
+
+    public int GetMaxMP() {
+        return pMaxMP;
+    }
+    public void SetMaxMP(int addMaxMP) {
+        pMaxMP += addMaxMP;
+    }
+    
 
 }
 
